74ae087968d9206695b5855e64bc31c0cfffb823
GamingLyon <73323583+AryanSuhasKhul@users.noreply.github.com>
2022-10-11 09:30:29 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,34 @@
+name "lyon-bodycam"
+author "GamingLyon"
+version "v0.0.1"
+description "Bodycam Script For Police By GamingLyon"
+
+fx_version 'cerulean'
+
+game 'gta5'
+
+
+
+files {
+
+  'html/vhs.ttf',
+
+  'html/main.js',
+
+  'html/index.html',
+
+  'html/axon_blank.png',
+
+}
+
+
+
+ui_page 'html/index.html'
+
+
+
+server_script 'server/main.lua'
+
+
+
+client_script 'client/main.lua'
